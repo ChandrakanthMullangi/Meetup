654421ed8484f1a562707819ee899df459b2474d
chandrakanth <chandrakanthmullangi.developer@gmail.com>
2023-04-20 04:53:52 +0000

    first commit

---
+++ src/components/Header/style.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const Nav = styled.nav`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  min-height: 10vh;
+  padding: 20px;
+`
+export const Logo = styled.img`
+  width: 180px;
+  height: auto;
+`
+
+export default Logo
---
+++ src/components/Home/style.js
@@ -0,0 +1,60 @@
+import styled from 'styled-components'
+
+export const HomeContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  min-height: 80vh;
+  padding: 25px;
+`
+
+export const HomeHeading = styled.h1`
+  color: #334155;
+  font-size: 52px;
+  font-family: 'Roboto';
+  font-weight: 500;
+  margin-bottom: 0px;
+`
+export const HomePara = styled.p`
+  color: #475569;
+  font-size: 22px;
+  font-family: 'Roboto';
+  font-weight: 500;
+`
+
+export const Button = styled.button`
+  background-color: #3b82f6;
+  color: #ffffff;
+  font-family: 'Roboto';
+  font-weight: bold;
+  font-size: 16px;
+  border: none;
+  outline: none;
+  padding-top: 12px;
+  padding-bottom: 12px;
+  padding-left: 20px;
+  padding-right: 20px;
+  border-radius: 6px;
+  cursor: pointer;
+`
+
+export const Image = styled.img`
+  height: auto;
+  width: 1000px;
+  margin-top: 20px;
+`
+
+export const Name = styled.h1`
+  color: #2563eb;
+  font-size: 50px;
+  font-family: 'Roboto';
+  margin-bottom: 0px;
+`
+
+export const Topic = styled.p`
+  color: #334155;
+  font-size: 30px;
+  font-family: 'Roboto';
+  font-weight: bold;
+`
